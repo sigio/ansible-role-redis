7bf8f35d27c9b361d849d34c1646cd7f3e123ba8
Mark Janssen -- Sig-I/O Automatisering <mark@sig-io.nl>
2021-02-27 13:23:35 +0100

    Initial import redis+redis-sentinel

---
+++ templates/notify.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+# {{ ansible_managed}}
+# redis-notify.sh
+
+MAIL_FROM="{{redis_notify_from}}"
+MAIL_TO="redis_notify_to}}"
+
+if [ "$#" = "2" ]; then
+    mail_subject="Redis Notification"
+    mail_body=`cat << EOB
+============================================
+Redis Notification Script called by Sentinel
+============================================
+
+Event Type: ${1}
+Event Description: ${2}
+
+Check the redis status.
+
+EOB`
+
+    echo "${mail_body}" | mail -r "${MAIL_FROM}" -s "${mail_subject}" "${MAIL_TO}"
+
+fi
+
